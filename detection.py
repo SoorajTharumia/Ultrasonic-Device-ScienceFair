41d79bb047a40d20ef516b1fc346bf4cd2170ecf
Sooraj Tharumia <sendsooraj@gmail.com>
2022-03-05 22:18:18 -0500

    commit

--- detection.py
+++ detection.py
@@ -10,7 +10,6 @@ import RPi.GPIO as GPIO
 import time
 from espeak import espeak
 
-# Sets up the GPIO devices and connections
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
@@ -28,34 +27,45 @@ time.sleep(0.5)
 servo1.start(0)
 
 # Define VideoStream class to handle streaming of video from webcam in separate processing thread
+# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
 class VideoStream:
     """Camera object that controls video streaming from the Picamera"""
     def __init__(self,resolution=(640,480),framerate=30):
+        # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
             
+        # Read first frame from the stream
         (self.grabbed, self.frame) = self.stream.read()
 
+	# Variable to control when the camera is stopped
         self.stopped = False
 
     def start(self):
+	# Start the thread that reads frames from the video stream
         Thread(target=self.update,args=()).start()
         return self
 
     def update(self):
+        # Keep looping indefinitely until the thread is stopped
         while True:
+            # If the camera is stopped, stop the thread
             if self.stopped:
+                # Close camera resources
                 self.stream.release()
                 return
 
+            # Otherwise, grab the next frame from the stream
             (self.grabbed, self.frame) = self.stream.read()
 
     def read(self):
+	# Return the most recent frame
         return self.frame
 
     def stop(self):
+	# Indicate that the camera and thread should be stopped
         self.stopped = True
 
 # Define and parse input arguments
@@ -82,28 +92,39 @@ resW, resH = args.resolution.split('x')
 imW, imH = int(resW), int(resH)
 use_TPU = args.edgetpu
 
+# Import TensorFlow libraries
+# If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflowq
 pkg = importlib.util.find_spec('tflite_runtime')
 if pkg:
     from tflite_runtime.interpreter import Interpreter
 else:
     from tensorflow.lite.python.interpreter import Interpreter
 
+# Get path to current working directory
 CWD_PATH = os.getcwd()
 
+# Path to .tflite file, which contains the model that is used for object detection
 PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
 
+# Path to label map file
 PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
 
+# Load the label map
 with open(PATH_TO_LABELS, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
+# Have to do a weird fix for label map if using the COCO "starter model" from
+# https://www.tensorflow.org/lite/models/object_detection/overview
+# First label is '???', which has to be removed.
 if labels[0] == '???':
     del(labels[0])
 
+# Load the Tensorflow Lite model.
 interpreter = Interpreter(model_path=PATH_TO_CKPT)
 
 interpreter.allocate_tensors()
 
+# Get model details
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 height = input_details[0]['shape'][1]
@@ -114,31 +135,42 @@ floating_model = (input_details[0]['dtype'] == np.float32)
 input_mean = 127.5
 input_std = 127.5
 
+# Initialize video stream
 videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
 time.sleep(1)
 
+#for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 while True:
 
+    # Grab frame from video stream
     frame1 = videostream.read()
 
+    # Acquire frame and resize to expected shape [1xHxWx3]
     frame = frame1.copy()
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_resized = cv2.resize(frame_rgb, (width, height))
     input_data = np.expand_dims(frame_resized, axis=0)
 
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
     if floating_model:
         input_data = (np.float32(input_data) - input_mean) / input_std
 
+    # Perform the actual detection by running the model with the image as input
     interpreter.set_tensor(input_details[0]['index'],input_data)
     interpreter.invoke()
 
-    boxes = interpreter.get_tensor(output_details[0]['index'])[0] 
-    classes = interpreter.get_tensor(output_details[1]['index'])[0] 
-    scores = interpreter.get_tensor(output_details[2]['index'])[0] 
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
+    #num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
     
+    # Loop over all detections and draw detection box if confidence is above minimum threshold
     for i in range(len(scores)):
         if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
 
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
             ymin = int(max(1,(boxes[i][0] * imH)))
             xmin = int(max(1,(boxes[i][1] * imW)))
             ymax = int(min(imH,(boxes[i][2] * imH)))
@@ -146,13 +178,13 @@ while True:
             
             cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
-            
-            object_name = labels[int(classes[i])]
-            label = '%s: %d%%' % (object_name, int(scores[i]*100))
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-            label_ymin = max(ymin, labelSize[1] + 10)
-            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
-            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+            # Draw label
+            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
             # Place dot in the center of the screen
             center_x = 640
@@ -173,26 +205,24 @@ while True:
                 direction = 0 # left
             else: 
                 direction = 1 # right
-            
-            # Calculate pixel difference in the screen and translate to an angle
+                
             pixel_difference_servo = abs(center_x - xcenter)
             angle_difference = pixel_difference_servo/10.5  
             
-            # Rotation of the servo motor holder
             if (direction == 0 ):
                 angle = 90 + angle_difference
                 
                 servo1.ChangeDutyCycle(2+(angle/18))
                 time.sleep(0.5)
-                servo1.ChangeDutyCycle(0)  
+                servo1.ChangeDutyCycle(0)
+                
             else:
                 angle = 90 - angle_difference
                 
                 servo1.ChangeDutyCycle(2+(angle/18))
                 time.sleep(0.5)
                 servo1.ChangeDutyCycle(0)
-
-            # Distance Measurement      
+                            
             GPIO.setmode(GPIO.BCM)
     
             pinTrigger = 18
@@ -223,73 +253,18 @@ while True:
             
             time.sleep(0.5)
             
-            # Text to Speech synthesis (tells the user where the object is)
-            if (direction == 0) and (distance == 1):
-                espeak.synth("There is an object to your left 1 meter away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 2):
-                espeak.synth("There is an object to your left 2 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 3):
-                espeak.synth("There is an object to your left 3 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 4):
-                espeak.synth("There is an object to your left 4 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 5):
-                espeak.synth("There is an object to your left 5 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 6):
-                espeak.synth("There is an object to your left 6 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 7):
-                espeak.synth("There is an object to your left 7 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 8):
-                espeak.synth("There is an object to your left 8 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 9):
-                espeak.synth("There is an object to your left 9 meters away")
-                time.sleep(2)
-            elif (direction == 0) and (distance == 10):
-                espeak.synth("There is an object to your left 10 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 1):
-                espeak.synth("There is an object to your right 1 meter away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 2):
-                espeak.synth("There is an object to your right 2 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 3):
-                espeak.synth("There is an object to your right 3 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 4):
-                espeak.synth("There is an object to your right 4 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 5):
-                espeak.synth("There is an object to your right 5 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 6):
-                espeak.synth("There is an object to your right 6 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 7):
-                espeak.synth("There is an object to your right 7 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 8):
-                espeak.synth("There is an object to your right 8 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 9):
-                espeak.synth("There is an object to your right 9 meters away")
-                time.sleep(2)
-            elif (direction == 1) and (distance == 10):
-                espeak.synth("There is an object to your right 10 meters away")
-                time.sleep(2)
+            # F string literal to dictate the direction and distance of the object
+            espeak.synth(f'There is an object to your {"left" if not direction else "right"} {distance} meters away')
+            time.sleep(2)
 
+    # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Obstruction Watcher', frame)
         
+    # Press 'q' to quit
     if cv2.waitKey(1) == ord('q'):
         break
 
+# Clean up
 cv2.destroyAllWindows()
 videostream.stop()
 GPIO.cleanup()
